5e4b59d9afd7654cc162ee838d331742b6348018
ezramizrahi <mizrahi.ez@gmail.com>
2022-12-13 12:29:04 +1100

    add pivotindex

---
+++ javascript/pivotIndex.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var pivotIndex = function(nums) {
+    let totalSum = 0;
+    let leftSum = 0;
+    nums.forEach((num) => totalSum += num);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (totalSum - leftSum - nums[i] === leftSum) {
+            return i;
+        };
+        leftSum += nums[i];
+    };
+    return -1;
+};
