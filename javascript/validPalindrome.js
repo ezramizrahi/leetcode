f0051f266f88550fce00aa859f8c58b8b86127ba
ezramizrahi <mizrahi.ez@gmail.com>
2022-12-13 14:53:09 +1100

    add pal

---
+++ javascript/validPalindrome.js
@@ -0,0 +1,18 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isPalindrome = function(s) {
+    if ( s === ' ' ) return true;
+    let removedNonAlphaNumericChars = s.replace(/\W/g, '').toLowerCase();
+    let reversed = removedNonAlphaNumericChars.split('').reverse().join('').toLowerCase();
+    if ( removedNonAlphaNumericChars === reversed ) {
+        return true;
+    } else {
+        return false;
+    }
+};
