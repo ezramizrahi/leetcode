a20d48b3df965d3ff81c2799d5a2d13ed4cdb2d0
ezramizrahi <mizrahi.ez@gmail.com>
2022-12-13 12:33:01 +1100

    update files

--- javascript/numberOfSteps.js
+++ javascript/numberOfSteps.js
@@ -1,4 +1,12 @@
-var numberOfSteps = function(num) {
+// Given an integer num, return the number of steps to reduce it to zero.
+
+// In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+ var numberOfSteps = function(num) {
     let count = 0;
     while (num > 0) {
         if (num % 2 === 0) {
