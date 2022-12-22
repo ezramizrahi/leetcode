737cda7aceccad5e6de1235194bec29e5cdcbac4
ezramizrahi <mizrahi.ez@gmail.com>
2022-12-22 17:11:47 +1100

    add valid paren leetcode

---
+++ python/validParentheses.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        expected_closing_brackets_stack = []
+        if len(s) % 2 != 0:
+            return False
+        dict = {'(' : ')', '[' : ']', '{' : '}'}
+        for char in s:
+            if char in dict.keys():
+                expected_closing_brackets_stack.append(char)
+            else:
+                if expected_closing_brackets_stack == []:
+                    return False
+                a = expected_closing_brackets_stack.pop()
+                if char != dict[a]:
+                    return False
+        return expected_closing_brackets_stack == []
