cf10c294a2129132f468da2285898865fbb0c885
ezramizrahi <mizrahi.ez@gmail.com>
2022-12-31 11:37:22 +1100

    add js file

---
+++ javascript/containsDuplicates.js
@@ -0,0 +1,11 @@
+/**
+ * Given an integer array nums, 
+ * return true if any value appears at least twice in the array, 
+ * and return false if every element is distinct.
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    const numSet = new Set(nums);
+    return nums.length != numSet.size;
+};
