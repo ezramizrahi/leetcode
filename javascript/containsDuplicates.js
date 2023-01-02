20dc5c9e653c8cf61025dfd05b723c3722de0028
ezramizrahi <mizrahi.ez@gmail.com>
2023-01-02 13:21:50 +1100

    maybe it's better to compare array against array

--- javascript/containsDuplicates.js
+++ javascript/containsDuplicates.js
@@ -1,12 +1,20 @@
+// Given an integer array nums, 
+// and return true if any value appears at least twice in the array, 
+// and return false if every element is distinct.
+
 /**
- * Given an integer array nums, 
- * return true if any value appears at least twice in the array, 
- * and return false if every element is distinct.
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const numSet = new Set(nums);
-    if (nums.length != numSet.size) return true;
+    const numsSet = new Set(nums);
+    if (nums.length != numsSet.size) return true;
     return false;
-};
+ };
+
+// or is this better
+// var containsDuplicate = function(nums) {
+//     const uniqueNumsArray = Array.from(new Set(nums));
+//     if (nums.length != uniqueNumsArray.length) return true;
+//     return false;
+// };
